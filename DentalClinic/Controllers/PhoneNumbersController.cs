cd89a69e2a32c5642dcf855de00e7bc50fcd6e1f
Mariam Atef <mariamatef676@gmail.com>
2024-09-17 16:45:16 +0200

    Create phone number control

---
+++ DentalClinic/Controllers/PhoneNumbersController.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using DentalClinic.Models; 
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace DentalClinic.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class PhoneNumbersController : ControllerBase
+    {
+        private readonly ClinicContext _context;
+
+        public PhoneNumbersController(ClinicContext context)
+        {
+            _context = context;
+        }
+
+
+
+
+
+    }
+
+
+}
