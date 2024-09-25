9af96e9bd8aff1ea94df5399d5912e91c099aa89
Karimashour <https://github.com/Karimashour>
2024-09-25 05:12:59 +0300

    change password model

---
+++ DentalClinic/Models/ChangePasswordModel.cs
@@ -0,0 +1,17 @@
+﻿using System.ComponentModel.DataAnnotations;
+namespace DentalClinic.Models
+{
+    public class ChangePasswordModel
+    {
+        [Required]
+        public string CurrentPassword { get; set; }
+
+        [Required]
+        [StringLength(100, MinimumLength = 6)]
+        public string NewPassword { get; set; }
+
+        [Required]
+        [Compare("NewPassword", ErrorMessage = "New password and confirmation password do not match.")]
+        public string ConfirmNewPassword { get; set; }
+    }
+}
