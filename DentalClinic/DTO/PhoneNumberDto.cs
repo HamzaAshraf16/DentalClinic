d66ec14974df6b44a9ca04fd53161c88fc398289
Aly Abo Zaid <ali.abozaidazz@gmail.com>
2024-10-04 01:29:49 +0300

    edit

--- DentalClinic/DTO/PhoneNumberDto.cs
+++ DentalClinic/DTO/PhoneNumberDto.cs
@@ -1,38 +1,20 @@
-﻿using System.ComponentModel.DataAnnotations;
-using System.ComponentModel.DataAnnotations.Schema;
+﻿using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
 
-namespace DentalClinic.DTO
+public class PhoneNumberDto
 {
-    public class PhoneNumberDto
-    {
-        [Required]
-        [RegularExpression(@"^(?:\+20|0)?1[0125]\d{8}$", ErrorMessage = "عليك إدخال رقم هاتف مصري مكون من 11 رقم")]
-        public string Phonenumber { get; set; }
+    [Required]
+    [RegularExpression(@"^(?:\+20|0)?1[0125]\d{8}$", ErrorMessage = "عليك إدخال رقم هاتف مصري مكون من 11 رقم")]
+    public string Phonenumber { get; set; }
 
-        [Required]
-        public int BranchID { get; set; }
-<<<<<<< HEAD
-
-        [Required(ErrorMessage = "Lacation is required")]
-        [Column(TypeName = "nvarchar")]
-        [StringLength(30)]
-        public string location {  get; set; }
-
-        [Required(ErrorMessage = "BranchName is required")]
-        [Column(TypeName = "nvarchar")]
-        [StringLength(30)]
-        public string BranchName {  get; set; }
-=======
-        [Required(ErrorMessage = "Lacation is required")]
-        [Column(TypeName = "nvarchar")]
-        [StringLength(100)]
-        public string BranchLocation { get; set; }
-
-        [Required(ErrorMessage = "Name is required")]
-        [Column(TypeName = "nvarchar")]
-        [StringLength(30)]
-        public string BranchName { get; set; }
->>>>>>> b8a636330d9b86b565486406fe7823144c752b09
-    }
+    [Required]
+    public int BranchID { get; set; }
+    [Required(ErrorMessage = "Lacation is required")]
+    [Column(TypeName = "nvarchar")]
+    [StringLength(100)]
+    public string BranchLocation { get; set; }
+    [Required(ErrorMessage = "Name is required")]
+    [Column(TypeName = "nvarchar")]
+    [StringLength(30)]
+    public string BranchName { get; set; }
 }
-
