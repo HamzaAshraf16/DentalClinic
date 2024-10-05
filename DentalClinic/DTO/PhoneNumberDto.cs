9dc071938e60bc16a3d335e037d79a3f48056508
HamzaAshraf16 <sherifhamza908@gmail.com>
2024-10-06 01:15:41 +0300

    update DoctorWbranch

--- DentalClinic/DTO/PhoneNumberDto.cs
+++ DentalClinic/DTO/PhoneNumberDto.cs
@@ -3,30 +3,9 @@ using System.ComponentModel.DataAnnotations;
 
 public class PhoneNumberDto
 {
-<<<<<<< HEAD
-    public class PhoneNumberDto
-    {
-        [Required]
-        [RegularExpression(@"^(?:\+20|0)?1[0125]\d{8}$", ErrorMessage = "عليك إدخال رقم هاتف مصري مكون من 11 رقم")]
-        public string Phonenumber { get; set; }
-
-        [Required]
-        public int BranchID { get; set; }
-        [Required(ErrorMessage = "Lacation is required")]
-        [Column(TypeName = "nvarchar")]
-        [StringLength(100)]
-        public string BranchLocation { get; set; }
-        [Required(ErrorMessage = "Name is required")]
-        [Column(TypeName = "nvarchar")]
-        [StringLength(30)]
-        public string BranchName { get; set; }
-    }
-}
-=======
     [Required]
     [RegularExpression(@"^(?:\+20|0)?1[0125]\d{8}$", ErrorMessage = "عليك إدخال رقم هاتف مصري مكون من 11 رقم")]
     public string Phonenumber { get; set; }
->>>>>>> f6a47c0f488a96134a8d779b231156da6638cc50
 
     [Required]
     public int BranchID { get; set; }
