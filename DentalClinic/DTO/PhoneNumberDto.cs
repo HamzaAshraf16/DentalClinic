03d41c730979e217109acda4dca9b390f06a2056
Omartaqi <117651069+Omartaqi@users.noreply.github.com>
2024-10-06 01:05:23 +0300

    Update PhoneNumberDto.cs

--- DentalClinic/DTO/PhoneNumberDto.cs
+++ DentalClinic/DTO/PhoneNumberDto.cs
@@ -1,9 +1,9 @@
-﻿using System.ComponentModel.DataAnnotations.Schema;
 using System.ComponentModel.DataAnnotations;
 
-public class PhoneNumberDto
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace DentalClinic.DTO
 {
-<<<<<<< HEAD
     public class PhoneNumberDto
     {
         [Required]
@@ -16,26 +16,10 @@ public class PhoneNumberDto
         [Column(TypeName = "nvarchar")]
         [StringLength(100)]
         public string BranchLocation { get; set; }
+
         [Required(ErrorMessage = "Name is required")]
         [Column(TypeName = "nvarchar")]
         [StringLength(30)]
         public string BranchName { get; set; }
     }
-}
-=======
-    [Required]
-    [RegularExpression(@"^(?:\+20|0)?1[0125]\d{8}$", ErrorMessage = "عليك إدخال رقم هاتف مصري مكون من 11 رقم")]
-    public string Phonenumber { get; set; }
->>>>>>> f6a47c0f488a96134a8d779b231156da6638cc50
-
-    [Required]
-    public int BranchID { get; set; }
-    [Required(ErrorMessage = "Lacation is required")]
-    [Column(TypeName = "nvarchar")]
-    [StringLength(100)]
-    public string BranchLocation { get; set; }
-    [Required(ErrorMessage = "Name is required")]
-    [Column(TypeName = "nvarchar")]
-    [StringLength(30)]
-    public string BranchName { get; set; }
 }
