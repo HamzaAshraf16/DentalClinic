0ec16c370fe6a675a495837d31d9356dd5a811a7
Karimashour <77418051+Karimashour@users.noreply.github.com>
2024-10-19 01:45:49 +0300

    Update ChatHub.cs

--- DentalClinic/Hubs/ChatHub.cs
+++ DentalClinic/Hubs/ChatHub.cs
@@ -1,39 +1,41 @@
-﻿using Microsoft.AspNetCore.SignalR;
-
+using Microsoft.AspNetCore.SignalR;
+using System.Collections.Concurrent;
 
 namespace DentalClinic.Hubs
 {
     public class ChatHub : Hub
     {
-        private static Dictionary<string, string> UserConnections = new Dictionary<string, string>();
+        private static ConcurrentDictionary<string, string> UserConnections = new ConcurrentDictionary<string, string>();
 
         public override async Task OnConnectedAsync()
         {
             string userType = Context.GetHttpContext().Request.Query["userType"];
-            UserConnections[Context.ConnectionId] = userType;
+            if (!string.IsNullOrEmpty(userType))
+            {
+                UserConnections.TryAdd(Context.ConnectionId, userType);
+                Console.WriteLine($"User connected: {Context.ConnectionId}, Type: {userType}");
+            }
             await base.OnConnectedAsync();
         }
 
         public override async Task OnDisconnectedAsync(Exception exception)
         {
-            UserConnections.Remove(Context.ConnectionId);
+            UserConnections.TryRemove(Context.ConnectionId, out _);
             await base.OnDisconnectedAsync(exception);
         }
 
         public async Task SendMessageToDoctor(string patientName, string message)
         {
-            foreach (var connection in UserConnections)
-            {
-                if (connection.Value == "Doctor")
-                {
-                    await Clients.Client(connection.Key).SendAsync("ReceiveMessage", patientName, message);
-                }
-            }
+
+
+            await Clients.All.SendAsync("ReceiveMessage", patientName, message);
+
+
         }
 
         public async Task SendMessageToPatient(string doctorName, string message, string patientConnectionId)
         {
-            await Clients.Client(patientConnectionId).SendAsync("ReceiveMessage", doctorName, message);
+            await Clients.All.SendAsync("ReceiveMessage", doctorName, message, patientConnectionId);
         }
     }
 }
